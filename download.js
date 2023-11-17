567a95b4dc7ae2958cf073edbda5a6e8508f0768
oc4t <150529784+oc4t@users.noreply.github.com>
2023-11-17 16:06:19 +0100

    Add files via upload

---
+++ download.js
@@ -0,0 +1,22 @@
+function downloadMods1202() {
+    const selectedMods1202 = Array.from(document.querySelectorAll('.mod1202:checked'))
+        .map(checkbox => checkbox.getAttribute('data-mod'));
+    if (selectedMods1202.length === 0) {
+        alert('Select at least 1 mod.');
+        return;
+    }
+
+    const zip = new JSZip();
+    const promises = selectedMods1202.map(modPath => {
+        const modName = modPath.split('/').pop();
+        const modFilePath = `mods/1.20.2/${modName}`;
+        return fetch(modFilePath)
+            .then(response => response.blob())
+            .then(blob => zip.file(modName, blob));
+    });
+
+    Promise.all(promises)
+        .then(() => zip.generateAsync({ type: 'blob' }))
+        .then(content => saveAs(content, 'mods.zip'))
+        .catch(error => console.error(error));
+}
---
+++ social.js
@@ -0,0 +1,3 @@
+function redirectDiscord() {
+  window.open("https://discord.gg/SuNtNuvBb5", "_blank");
+}
